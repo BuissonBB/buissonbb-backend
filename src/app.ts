b5a99a9570f71c45c66b68661f2601a297cca0a8
Feavy <quentin071100@live.fr>
2021-11-29 22:06:28 +0100

    Add static assets

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,21 @@ import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio';
 import services from './services';
 import configuration from '@feathersjs/configuration';
+import express from '@feathersjs/express';
 import '@feathersjs/transport-commons';
+import path from 'path';
 
 import knex from './knex';
 
 const PORT = process.env.PORT || 3030;
 
 // Create a Feathers application
-const app = feathers();
+const app = express(feathers());
+
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
 // Configure the Socket.io transport
-app.configure(socketio())
+app.configure(socketio());
 
 // Create a channel that will handle the transportation of all realtime events
 app.on('connection', connection => app.channel('everybody').join(connection));
